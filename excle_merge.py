57d050d92313c7db2df5afd4fe367aad9e7dd3ec
bmichas <michasbartek@gmail.com>
2020-10-05 11:58:05 +0200

    Dodano: -Generowanie pliku połączonego -Generowanie do excel

--- excle_merge.py
+++ excle_merge.py
@@ -1,4 +1,14 @@
 import pandas as pd
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+
+def variables():
+    global buy, sell, connect_file
+    buy = 'buy.xlsx'
+    sell = 'sell.xlsx'
+    connect_file = 'main_data.xlsx'
 
 
 def df_generator(file):
@@ -13,11 +23,11 @@ def df_generator(file):
     return df
 
 
-def variables():
-    global buy, sell, merge_file
-    buy = 'buy.xlsx'
-    sell = 'sell.xlsx'
-    merge_file = 'main_data.xlsx'
+def file_merge(main_file, first, second):
+    first_connect_file = pd.merge(first, main_file)
+    merge_file = pd.merge(first_connect_file, second)
+    merge_file.to_excel("merge.xlsx")
+    return merge_file
 
 
 def main():
@@ -25,9 +35,11 @@ def main():
     # generowanie DataFrame
     df_buy = df_generator(buy)
     df_sell = df_generator(sell)
-    df_merge_file = df_generator(merge_file)
-    print(df_buy)
-    print(df_sell)
+    df_connect_file = df_generator(connect_file)
+    # print(df_buy)
+    # print(df_sell)
+    # print(df_connect_file)
+    df_merge_file = file_merge(df_connect_file, df_buy, df_sell)
     print(df_merge_file)
 
 
